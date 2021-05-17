f7ff3c2b4e9e52847afeb0c161bfcc7714fe8868
Aks Sreenivasan <aks.auburn@gmail.com>
2021-05-17 15:31:23 +1000

    heroku ready

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 var cors = require ('cors');
-
+const path = require("path");
 // routes
 const songs = require('./routes/api/songs');
 const app = express();
@@ -14,10 +14,15 @@ app.use(cors({ origin: true, credentials: true }));
 
 //Init Middleware
 app.use(express.json({ extended: false }));
-app.get('/', (req, res) => res.send('Hello world!'));
+//app.get('/', (req, res) => res.send('Hello world!'));
+app.use(express.static(path.join(__dirname,"client","build")))
 
 //use Routes
 app.use('/api/songs', songs);
 const port = process.env.PORT || 8082;
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
